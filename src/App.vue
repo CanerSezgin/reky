717293c77efba39324e11da9fd4f0a53aad12746
Caner Sezgin <ccanersezgin@gmail.com>
2020-08-05 19:12:21 +0300

    vue upgrading

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content class="mx-5">
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.longtext {
+  word-wrap: break-word;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import VueAnalytics from "vue-analytics";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: "UA-145060126-1",
+});
 
 new Vue({
   router,
   store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  vuetify,
+  render: function(h) {
+    return h(App);
+  },
+}).$mount("#app");
