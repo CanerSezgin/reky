949e153f921216ff1a5a44e929e254d87c07d3bd
Caner Sezgin <ccanersezgin@gmail.com>
2020-08-05 14:29:21 +0300

    new vue project from scratch

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
-  <v-app>
-    <v-content class="mx-5">
-      <router-view/>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-
-export default {
-  name: 'App',
-  data: () => ({
-    //
-  }),
-};
-</script>
-
 <style>
-.longtext {
-    word-wrap: break-word;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
-
--- src/main.js
+++ src/main.js
@@ -2,18 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
-import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
-
-Vue.use(VueAnalytics, {
-  id: 'UA-145060126-1'
-})
 
 new Vue({
   router,
   store,
-  vuetify,
   render: function (h) { return h(App) }
 }).$mount('#app')
